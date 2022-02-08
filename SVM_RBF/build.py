fbc4ca0681f46e5b70f76df3a02a87bcc89c60db
uingrd <uingrd@outlook.com>
2022-02-08 21:05:18 +0800

    build.py支持Linux上编译执行

--- SVM_RBF/build.py
+++ SVM_RBF/build.py
@@ -1,8 +1,13 @@
-
-import sys,os
-
-os.system('clang mat_f32.c -Wall -c -o export_code/mat_f32.o -fno-stack-protector')
-os.system('clang svm_rbf.c -Wall -c -o export_code/svm_rbf.o -fno-stack-protector')
-os.system('clang export_code/svm_rbf_test.c -Wall -c -o export_code/svm_rbf_test.o -fno-stack-protector')
-os.system('clang export_code/mat_f32.o export_code/svm_rbf.o export_code/svm_rbf_test.o -Wall -o export_code/test')
-os.system('export_code/test')
+import platform,os
+if platform.system()=='Darwin': # MacOS
+    os.system('clang mat_f32.c -Wall -c -o export_code/mat_f32.o -fno-stack-protector')
+    os.system('clang svm_rbf.c -Wall -c -o export_code/svm_rbf.o -fno-stack-protector')
+    os.system('clang export_code/svm_rbf_test.c -Wall -c -o export_code/svm_rbf_test.o -fno-stack-protector')
+    os.system('clang export_code/mat_f32.o export_code/svm_rbf.o export_code/svm_rbf_test.o -Wall -o export_code/test')
+    os.system('export_code/test')
+else:
+    os.system('gcc mat_f32.c -Wall -c -o export_code/mat_f32.o -fno-stack-protector')
+    os.system('gcc svm_rbf.c -Wall -c -o export_code/svm_rbf.o -fno-stack-protector')
+    os.system('gcc export_code/svm_rbf_test.c -Wall -c -o export_code/svm_rbf_test.o -fno-stack-protector')
+    os.system('gcc export_code/mat_f32.o export_code/svm_rbf.o export_code/svm_rbf_test.o -Wall -lm -o export_code/test')
+    os.system('export_code/test')
--- SVM_RBF/readme.md
+++ SVM_RBF/readme.md
@@ -8,7 +8,7 @@
     - mat_f32.c, mat_f32.h: 矩阵运算代码
     - export_code/svm_rbf_test.c, export_code/svm_rbf_test.h: 测试数据文件(svm_rbf.py自动生成)
 4. 备注:
-    - build.py目前仅测试了在macOS上的执行
+    - build.py目前仅测试了在Ubuntu和macOS上的执行
     - 文件svm_rbf.c, svm_rbf.h, mat_f32.c, mat_f32.h在ARM的CMSIS库中有对应的实现，可以通过编译选项 -DUSE_CMSIS 选择使用CMSIS库的实现
     - 虽然C测试代码在PC上运行，该程序能够直接移植到嵌入式系统实现
     - 测试代码的数据验证精度是93.567251%
