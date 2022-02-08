7de0bb08736f8664cb852ce7c393e1ee835566cb
uingrd <uingrd@outlook.com>
2022-02-08 21:14:45 +0800

    增加Linux下的编译脚本（build.py）支持

--- MCM/build.py
+++ MCM/build.py
@@ -1,7 +1,11 @@
-
-import sys,os
-
-os.system('clang float_shift.c -Wall -c -o export_code/float_shift.o -fno-stack-protector')
-os.system('clang export_code/auto_code.c -Wall -c -o export_code/auto_code.o -fno-stack-protector')
-os.system('clang export_code/float_shift.o export_code/auto_code.o -Wall -o export_code/test')
-os.system('export_code/test')
+import platform,os
+if platform.system()=='Darwin': # MacOS
+    os.system('clang float_shift.c -Wall -c -o export_code/float_shift.o -fno-stack-protector')
+    os.system('clang export_code/auto_code.c -Wall -c -o export_code/auto_code.o -fno-stack-protector')
+    os.system('clang export_code/float_shift.o export_code/auto_code.o -Wall -o export_code/test')
+    os.system('export_code/test')
+else:
+    os.system('gcc float_shift.c -Wall -c -o export_code/float_shift.o -fno-stack-protector')
+    os.system('gcc export_code/auto_code.c -Wall -c -o export_code/auto_code.o -fno-stack-protector')
+    os.system('gcc export_code/float_shift.o export_code/auto_code.o -Wall -o export_code/test')
+    os.system('export_code/test')
