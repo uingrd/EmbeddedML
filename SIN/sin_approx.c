7d4bc1bee3ad6ba3de32cc7b6745f61225f483f1
uingrd <uingrd@outlook.com>
2024-05-07 02:14:54 +0000

    Update sin_approx.c
    bugfix

--- SIN/sin_approx.c
+++ SIN/sin_approx.c
@@ -5,6 +5,8 @@
 #define PI 3.1415926535897932384626433832795
 #define SIGN(x) (((x)>=0)?((x)>0):-1)
 #define ABS(x) ((x)>0?(x):(-(x)))
+#define DEG2RAD(x) (x)*(float)(PI/180.0)
+#define RAD2DEG(x) (x)*(float)(180.0/PI)
 
 float algo1(float x)
 {
@@ -24,30 +26,35 @@ float algo3(float x)
 {
     return 16.0f*x*((float)PI-x)/((float)(5.0*PI*PI)-4.0f*x*((float)PI-x));
 }
- 
+
+float algo4_deg(float d)
+{
+    float d1=d*(180.0f-d);
+    return (27900.0f*d1+d1*d1)/291600000.0f;
+}
+
+float algo4(float x)
+{
+    return algo4_deg(RAD2DEG(x));
+}
+
 typedef float (*algo_t)(float);
 
-// 计算ARCTAN近似，使用不同的算法
-// 输入数据范围是-PI~+PI
+#include <stdio.h>
+#include <math.h>
+// 计算SIN近似，使用不同的算法
 float sin_approx(float x, algo_t algo)
 {
-    if ((x>(float)(PI*2.0f)) || (x<-(float)(PI*2.0f)))
-    {
-        int k=x/(float)(PI*2.0f);
-        x-=(float)k*(float)(PI*2.0);
-    }
-    if (x>PI) x-=(float)(PI*2.0);
-    
-    return SIGN(x)*algo(ABS(x));
+    x=fmod(x,(float)(PI*2.0));
+    if (x<0) x+=(float)(PI*2.0);
+    return x>PI?-algo(x-PI):algo(x);
 }
 
 ////////////////////////
 // 单元测试
 ////////////////////////
 
-#include <math.h>
-#include <stdio.h>
-#define N 10000
+#define N 20000
 #define MAX(a,b) (((a)>(b))?(a):(b))
 
 // 测试近似算法误差
@@ -58,7 +65,10 @@ float test_algo(algo_t algo)
     {
         t=(float)(n-N/2)/(float)(N+1)*(float)M_PI*2.0f;
         err=MAX(err,ABS(sin_approx(t,algo)-sin(t)));
+        
+        //printf("%f,%f\n",sin(t),sin_approx(t,algo));
     }
+    //printf("\n");
     return err;
 }
 
@@ -67,6 +77,7 @@ int main()
     printf("algo1 err. max: %f\n", test_algo(algo1));
     printf("algo2 err. max: %f\n", test_algo(algo2));
     printf("algo3 err. max: %f\n", test_algo(algo3));
+    printf("algo4 err. max: %f\n", test_algo(algo4));
     
     return 0;
 }
